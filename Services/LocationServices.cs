234c981bad380b76773f118c58967e40e70366d6
Prithviraj S P <prithvi2410rj@gmail.com>
2022-04-21 14:17:55 +0530

    Update LocationServices.cs

--- Services/LocationServices.cs
+++ Services/LocationServices.cs
@@ -6,8 +6,8 @@ using System;
 public class LocationServices{
 
     //For Location-Entity
-    public void AddLocation(int locationId);
+    public void AddLocation(Location location);
     public void RemoveLocation(int locationId);
-    public Location ViewLocations(int locationId);
-    public List<Location> ViewLocation();
+    public Location ViewLocation(int locationId);
+    public List<Location> ViewLocations();
  };
