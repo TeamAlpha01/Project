5b36f0ff2e23f7071e62c95387fbc6138d4bbb51
Vinoth1704 <vinoth.1704j@gmail.com>
2022-04-23 12:16:18 +0530

    Updated Logical diagram

--- Services/DepartmentServices.cs
+++ Services/DepartmentServices.cs
@@ -8,5 +8,9 @@ public class DepartmentServices{
     public void CreateDepartment(Department department);
     public List<Department> ViewDepartment();
     public void RemoveDepartment(int departmentId);
+
+    public void CreateProject(Project project);
+    public void RemoveProject(int projectId);
+    public List<Project> ViewProjects(int departmentId);
     
 }
