d5e1b392b546ae4cc087ce9a31f190cba55abe07
Vinoth <vinoth.1704j@gmail.com>
2022-04-23 14:56:38 +0530

    Updated Services

--- Services/PoolServices.cs
+++ Services/PoolServices.cs
@@ -1,12 +1,18 @@
 using System;
-public class Pool{
-
-}
 
 
-public class PoolServices{
+public class PoolServices
+{
     public void CreatePool(Pool pool);
     public void EditPool(int poolId);
     public void RemovePool(int poolId);
     public List<Pool> ViewPool(int departmentId);
+}
+
+public class PoolMembersServices
+{
+    public void CreatePool(Employee employeeId);
+    public void RemovePool(int employeeId);
+    public List<Employee> ViewMembers(int departmentId, int employeeId);
+
 }
--- Services/RoleServices.cs
+++ Services/RoleServices.cs
@@ -1,10 +1,8 @@
 using System;
- public class Role{
 
- };
-
- public class RoleServices{
-     public void CreateRole(Role role);
-     public List<Role> ViewRoles();
-     public void RemoveRole(int roleId);
- };
+public class RoleServices
+{
+    public void CreateRole(Role role);
+    public List<Role> ViewRoles();
+    public void RemoveRole(int roleId);
+};
