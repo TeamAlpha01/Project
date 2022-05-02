e2da05eb9d4062469f6ddbb488909bc3ea665c52
Prithviraj S P <prithvi2410rj@gmail.com>
2022-05-02 11:54:37 +0530

    Updated Timesheet - Prithvi

---
+++ Source/InterviewManagementSystemAPI/Models/Drive.cs
@@ -0,0 +1,10 @@
+namespace InterviewManagementSystemAPI.Models
+{
+    public class Drive{
+        public int DriveId { get; set; }
+
+        public string Name { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
