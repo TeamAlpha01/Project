7a15ea17f57893fae39ffcb9f00607c428cd5f06
Vinoth <vinoth.1704j@gmail.com>
2022-05-03 09:38:46 +0530

    Updated interviewer header and footer

---
+++ Source/Angular/IMS/src/app/interviewer-header/interviewer-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interviewer-header',
+  templateUrl: './interviewer-header.component.html',
+  styleUrls: ['./interviewer-header.component.css']
+})
+export class InterviewerHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
