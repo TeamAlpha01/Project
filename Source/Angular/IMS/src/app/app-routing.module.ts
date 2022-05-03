739d315f1b14de9d6f7debc2af294dd591c1bceb
Vinoth <vinoth.1704j@gmail.com>
2022-05-03 12:20:29 +0530

    Updated components

--- Source/Angular/IMS/src/app/app-routing.module.ts
+++ Source/Angular/IMS/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminRoleComponent } from './admin-role/admin-role.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component:AdminRoleComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Source/Angular/IMS/src/app/interviewer-header/interviewer-header.component.ts
+++ Source/Angular/IMS/src/app/interviewer-header/interviewer-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-interviewer-header',
@@ -7,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterviewerHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
   }
   userName="Admin";
-
+  gotohome(){
+    this.router.navigate(['/home']);
+  }
 }
