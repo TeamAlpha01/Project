4e6c2c7f6a5c9d3991c7d7f5344ad4d6dd244442
Remuki2000 <aravindhanra2001.ra@gmail.com>
2022-05-04 09:23:00 +0530

    Updated

---
+++ Source/Angular/IMS/src/app/Admin/admin-addlocation/admin-addlocation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-addlocation',
+  templateUrl: './admin-addlocation.component.html',
+  styleUrls: ['./admin-addlocation.component.css']
+})
+export class AdminAddlocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
