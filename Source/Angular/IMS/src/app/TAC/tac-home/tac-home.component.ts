d883861777a91531d84dadaf0f9772bed814262a
deepikas19 <deepikasenthil7766@gmail.com>
2022-05-04 14:11:34 +0530

    Updated

---
+++ Source/Angular/IMS/src/app/TAC/tac-home/tac-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tac-home',
+  templateUrl: './tac-home.component.html',
+  styleUrls: ['./tac-home.component.css']
+})
+export class TacHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
