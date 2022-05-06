65afb1b80ec45de6210f41a8edd7cc619c0865fe
Aravindhan <aravindhanra2001.ra@gmail.com>
2022-05-06 10:35:04 +0530

    updated

--- Source/Angular/IMS/src/app/Admin/admin-adddepartment/admin-adddepartment.component.ts
+++ Source/Angular/IMS/src/app/Admin/admin-adddepartment/admin-adddepartment.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Department } from 'src/app/Model/Department';
 
 @Component({
   selector: 'app-admin-adddepartment',
