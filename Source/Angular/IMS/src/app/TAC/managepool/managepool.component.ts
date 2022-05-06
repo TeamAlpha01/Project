17f5f1a9fd8e8a1c72543bbf1e9f8a93b5ea8b20
Remuki2000 <remukisoundararajan@gmail.com>
2022-05-06 08:36:14 +0530

    updated

---
+++ Source/Angular/IMS/src/app/TAC/managepool/managepool.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-managepool',
+  templateUrl: './managepool.component.html',
+  styleUrls: ['./managepool.component.css']
+})
+export class ManagepoolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
