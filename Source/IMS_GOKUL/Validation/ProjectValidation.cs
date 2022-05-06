623fb78fac228d89576aee3ad159676740e356a6
gokul1306 <balagokul1306@gmail.com>
2022-05-07 00:34:15 +0530

    updated department service

--- Source/IMS_GOKUL/Validation/ProjectValidation.cs
+++ Source/IMS_GOKUL/Validation/ProjectValidation.cs
@@ -9,13 +9,25 @@ namespace IMS.Validation
 {
     public class ProjectValidation
     {
-          public static bool IsProjectValid(int departmentId,string projectName)
+          public static void IsProjectValid(int departmentId,string projectName)
         {
             if(departmentId <= 0)  throw new ValidationException("department name cannot be null");
             if(projectName==null) throw new ValidationException("project name cannot be null");
             //if(departmentName.Length<2 && departmentName.Length>15) throw new ValidationException("department name is in Invalid Length");
             if(!Regex.IsMatch(projectName,@"^[a-zA-Z]{3,15}$")) throw new ValidationException("Department name is invalid");
-            return true;
+            
         }
+        public static void IsProjectValid(int projectId)
+        {
+          if(projectId <=0) throw new ValidationException("Project Id Should not be Zero or Less than Zero");
+        }
+        public static void IsProjectValid(Project project)
+         {
+            if(project.DepartmentId <= 0)  throw new ValidationException("department Id cannot be Zero or Less than Zero");
+            if(project.ProjectName==null) throw new ValidationException("project name cannot be null");
+            //if(departmentName.Length<2 && departmentName.Length>15) throw new ValidationException("department name is in Invalid Length");
+            if(!Regex.IsMatch(project.ProjectName,@"^[a-zA-Z]{3,15}$")) throw new ValidationException("Project name is invalid");
+           
+         }
     }
 }
