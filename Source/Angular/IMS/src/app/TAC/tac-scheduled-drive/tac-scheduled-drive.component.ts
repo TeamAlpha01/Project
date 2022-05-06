7c143bcc5e8f675144e6b0dbc6ebdf2cf2f3bf45
deepikas19 <deepikasenthil7766@gmail.com>
2022-05-06 09:44:28 +0530

    Updated

---
+++ Source/Angular/IMS/src/app/TAC/tac-scheduled-drive/tac-scheduled-drive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tac-scheduled-drive',
+  templateUrl: './tac-scheduled-drive.component.html',
+  styleUrls: ['./tac-scheduled-drive.component.css']
+})
+export class TacScheduledDriveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
