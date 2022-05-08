e13d9b8e7135b964b02c2089718e903b316d4d44
Remuki2000 <remukisoundararajan@gmail.com>
2022-05-08 15:09:32 +0530

    updated

--- Source/Angular/IMS/src/app/TAC/managepoolmembers/managepoolmembers.component.ts
+++ Source/Angular/IMS/src/app/TAC/managepoolmembers/managepoolmembers.component.ts
@@ -1,3 +1,4 @@
+ 
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,183 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./managepoolmembers.component.css']
 })
 export class ManagepoolmembersComponent implements OnInit {
-
+    
+            
+  
   constructor() { }
 
   ngOnInit(): void {
-  }
-
+   
+      } 
+  
+  data1: any[]= [{
+    ACE_Number :'ACE001',
+    Name : 'Ajay',
+    Role:  'SE'
+  },{
+    ACE_Number :'ACE002',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE003',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE004',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  }]
+  
+  data2: any[]= [{
+    ACE_Number :'ACE001',
+    Name : 'Ajay',
+    Role:  'SE'
+  },{
+    ACE_Number :'ACE002',
+    Name : 'Ajay',
+    Role:  'SSE'
+  },
+  {
+    ACE_Number :'ACE003',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE004',
+    Name : 'Ajay',
+    Role:  'PM'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  },
+  {
+    ACE_Number :'ACE005',
+    Name : 'Ajay',
+    Role:  'SE'
+  }]
+  
 }
