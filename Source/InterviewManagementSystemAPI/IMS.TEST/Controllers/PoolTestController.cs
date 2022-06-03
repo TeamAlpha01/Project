a4a1f8eedb23ee29038fc7b775dc936b11fd4031
DarshanaAnnappan <darshana0175@gmail.com>
2022-06-03 13:28:24 +0530

    Update PoolTestController.cs

--- Source/InterviewManagementSystemAPI/IMS.TEST/Controllers/PoolTestController.cs
+++ Source/InterviewManagementSystemAPI/IMS.TEST/Controllers/PoolTestController.cs
@@ -10,12 +10,26 @@ using Xunit;
 namespace UnitTesting.Controllers;
 public class PoolControllerTest
 {
-private readonly PoolController _poolController;
-private readonly Mock<ILogger<PoolController>> _logger = new Mock<ILogger<PoolController>>();
+    private readonly PoolController _poolController;
+    private readonly Mock<ILogger<PoolController>> _logger = new Mock<ILogger<PoolController>>();
+    private readonly Mock<MailService> _mailService = new Mock<MailService>();
     private readonly Mock<IPoolService> _roleService = new Mock<IPoolService>();
     public PoolControllerTest()
     {
-        _poolController = new RoleController(_logger.Object, _poolService.Object);
+      _poolController=new PoolController(_logger.Object,_mailService.Object,_roleService.Object);
     }
+    //Test cases for create pool
+    [Theory]
+    [InlineData(0,null)]
+    
+
+    public void CreateNewPool_ShouldReturnStatusCode400_WhenRoleNameIsEmptyOrNull(int departmentId, string poolName)
+    {
+        
+        var Result = _poolController.CreateNewPool(departmentId,poolName) as ObjectResult;
+        
+        Result.StatusCode.Should().Be(400);
+    }
+
 
 }
