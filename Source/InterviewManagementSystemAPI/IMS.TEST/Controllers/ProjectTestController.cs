1bad0afbfc5eb2165a7b37ac1b800a1c9df3bec3
DarshanaAnnappan <darshana0175@gmail.com>
2022-06-11 13:16:48 +0530

    ProjectTestController

---
+++ Source/InterviewManagementSystemAPI/IMS.TEST/Controllers/ProjectTestController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using IMS.Controllers;
+using IMS.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+namespace UnitTesting.Controllers;
+
+public class ProjectTestController
+{
+    private readonly ProjectController _projectController;
+    private readonly Mock<ILogger<ProjectTestController>> _logger =new Mock<ILogger<ProjectTestController>>();
+    private readonly Mock<IDepartmentService> _departmentService =new Mock<IDepartmentService>();
+
+    public ProjectTestController()
+    {
+        _projectController=new ProjectController(_logger.Object,_departmentService.Object);
+
+    }
+    
+}
