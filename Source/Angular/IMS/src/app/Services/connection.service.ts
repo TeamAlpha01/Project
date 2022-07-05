9e950f528b60d61d16d81b0de6fd0740fc085328
deepikas19 <100795770+deepikas19@users.noreply.github.com>
2022-07-05 15:40:18 +0530

    Made some corrections

--- Source/Angular/IMS/src/app/Services/connection.service.ts
+++ Source/Angular/IMS/src/app/Services/connection.service.ts
@@ -97,7 +97,7 @@ export class ConnectionService {
   }
 
   GetTACDashboard(): any {
-    return this.http.get<any>(this.baseURL + `Drive/ViewDas hboard`, { headers: this.headers });
+    return this.http.get<any>(this.baseURL + `Drive/ViewDashboard`, { headers: this.headers });
   }
 
   GetTodayDrives(): any {
