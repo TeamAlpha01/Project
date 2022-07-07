0cac1f232ef5cc5b37ced5702c4bd24ce66e44d3
Prithviraj S P <prithvi2410rj@gmail.com>
2022-07-07 11:46:51 +0530

    Added Configuration Values in Drive validation

--- Source/InterviewManagementSystemAPI/IMS.API/Validations/DriveValidation.cs
+++ Source/InterviewManagementSystemAPI/IMS.API/Validations/DriveValidation.cs
@@ -8,28 +8,34 @@ namespace IMS.Validations
     {
         public static void IsdriveValid(Drive drive,IConfiguration configuration)
         {
-            int minimumFromDate = Convert.ToInt32(configuration.GetSection("MySettings").GetSection("DbConnection").Value); 
+            int minimumFromDate = Convert.ToInt32(configuration.GetSection("DriveSettings").GetSection("MinimumFromDate").Value); 
+            int maximumFromDate = Convert.ToInt32(configuration.GetSection("DriveSettings").GetSection("MaximumFromDate").Value); 
+            int drivePeriod = Convert.ToInt32(configuration.GetSection("DriveSettings").GetSection("DrivePeriod").Value); 
+
             if (drive == null) throw new ValidationException("drive object cannot be null");
 
             if (drive.Name.Length <= 2) throw new ValidationException("drive name is too short");
 
             //if(!Regex.IsMatch(drive.Name,"^[a-zA-Z0-9]+\\s[a-zA-Z][{2,20}$")) throw new ValidationException("drive name cannot contain symbols or numbers");
 
-            if (drive.FromDate.Date < System.DateTime.Now.Date.AddDays(7)) throw new ValidationException($"From date must be greater than {System.DateTime.Now.Date.AddDays(6).ToShortDateString()}");
+            if (drive.FromDate.Date < System.DateTime.Now.Date.AddDays(minimumFromDate)) throw new ValidationException($"From date must be greater than {System.DateTime.Now.Date.AddDays(minimumFromDate-1).ToShortDateString()}");
 
-            if (drive.FromDate.Date >= System.DateTime.Now.Date.AddMonths(1)) throw new ValidationException($"From date must be smaller than {System.DateTime.Now.Date.AddMonths(1).ToShortDateString()}");
+            if (drive.FromDate.Date >= System.DateTime.Now.Date.AddDays(maximumFromDate)) throw new ValidationException($"From date must be smaller than {System.DateTime.Now.Date.AddDays(maximumFromDate+1).ToShortDateString()}");
             
             if (drive.ToDate.Date < drive.FromDate.Date) throw new ValidationException("To date must be greater than from date");
 
-            if ((drive.ToDate.Date - drive.FromDate.Date ).Days > 7 ) throw new ValidationException($"The Drive period must be within Seven(7) Days");
+            if ((drive.ToDate.Date - drive.FromDate.Date ).Days > drivePeriod ) throw new ValidationException($"The Drive period must be within Seven(7) Days");
 
+            setDefaultValues(drive);
+        }
+        private static void setDefaultValues(Drive drive)
+        {
             //Setting Default values
             drive.IsScheduled = false;
             drive.IsCancelled = false;
             drive.AddedOn = DateTime.Now;
             drive.UpdatedOn = DateTime.Now;
             drive.CancelReason = null;
-
         }
         public static void IsCancelDriveValid(int driveId, int tacId, string reason)
         {
