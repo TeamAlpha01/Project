4df396b716bfa64a5cbe03672cc41ba6f7defb6d
AravindhanRa <100895574+AravindhanRa@users.noreply.github.com>
2022-07-07 14:25:13 +0530

    updated

--- Source/Angular/IMS/src/app/Management/poolmembersperformance/poolmembersperformance.component.ts
+++ Source/Angular/IMS/src/app/Management/poolmembersperformance/poolmembersperformance.component.ts
@@ -8,26 +8,25 @@ import { ConnectionService } from 'src/app/Services/connection.service';
   styleUrls: ['./poolmembersperformance.component.css']
 })
 export class PoolmembersperformanceComponent implements OnInit {
-   title="View Defaulters";
-   page: number=1;
-   poolName: any;
-  _pool = '';
+  title = "View Defaulters";
+  page: number = 1;
   totalLength: any;
-  item:any;
-
+  item: any;
+  employeesUnderPool: any;
+  poolName:any[]=[];
   constructor(private connection: ConnectionService) { }
 
   ngOnInit(): void {
-    this.GetPools();
-    
+    this.GetPoolsById();
+
   }
-  GetPools() {
-    this.connection.GetPools().subscribe((data: any) => {
-      this.poolName = data;
+  GetPoolsById() {
+    this.connection.GetPoolsById(this.employeesUnderPool).subscribe((data: any) => {
+      this.employeesUnderPool = data;
     })
   }
 
-  
 
- 
-  }
+
+
+}
