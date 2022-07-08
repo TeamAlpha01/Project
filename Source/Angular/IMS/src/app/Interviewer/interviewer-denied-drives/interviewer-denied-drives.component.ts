5ea1d1ac9fa3c5ecee27fc3286d9be7c6eded9f1
deepikas19 <100795770+deepikas19@users.noreply.github.com>
2022-07-08 09:35:33 +0530

    Updated - Filters in Interviewer dashboard

--- Source/Angular/IMS/src/app/Interviewer/interviewer-denied-drives/interviewer-denied-drives.component.ts
+++ Source/Angular/IMS/src/app/Interviewer/interviewer-denied-drives/interviewer-denied-drives.component.ts
@@ -13,6 +13,17 @@ export class InterviewerDeniedDrivesComponent implements OnInit {
   totalLength: any;
   page: number = 1;
 
+  _pool = '';
+  _date = '';
+
+  //To store the filtered data in the array
+  pool: any[] = [];
+  drive: any[] = [];
+
+  //To get the details from the db
+  driveDetails: any;
+  poolDetails: any;
+
   constructor(private connection :ConnectionService) { }
 
   ngOnInit(): void {
@@ -20,6 +31,30 @@ export class InterviewerDeniedDrivesComponent implements OnInit {
       this.Denied = data;
       console.warn(this.Denied)
     }) 
+
+    this.connection.GetPoolsbyId().subscribe((data: any) => {
+      this.poolDetails = data;
+      console.warn(this.poolDetails)
+    })
   }
 
+  filterDropdown() {
+  
+    this.drive = [];
+  
+    for (let item of this.Denied) {
+      if ( this._pool == '' && item.fromDate <= this._date && this._date <= item.toDate) {
+        this.drive.push(item);
+      }
+      else if (item.drivePool == this._pool && item.fromDate <= this._date && this._date <= item.toDate) {
+        this.drive.push(item);
+      }
+      else if (this._pool == '' && this._date == '') {
+        this.drive.push(item);
+      }
+      else if (item.drivePool == this._pool && this._date == '') {
+        this.drive.push(item);
+      }
+    }
+  }
 }
--- Source/Angular/IMS/src/app/Interviewer/interviewer-utilized-drives/interviewer-utilized-drives.component.ts
+++ Source/Angular/IMS/src/app/Interviewer/interviewer-utilized-drives/interviewer-utilized-drives.component.ts
@@ -13,6 +13,17 @@ export class InterviewerUtilizedDrivesComponent implements OnInit {
   totalLength: any;
   page: number = 1;
 
+  _pool = '';
+  _date = '';
+
+  //To store the filtered data in the array
+  pool: any[] = [];
+  drive: any[] = [];
+
+  //To get the details from the db
+  driveDetails: any;
+  poolDetails: any;
+
   constructor(private connection :ConnectionService) { }
 
   ngOnInit(): void {
@@ -21,6 +32,29 @@ export class InterviewerUtilizedDrivesComponent implements OnInit {
       console.warn(this.Utilized)
     }) 
 
+    this.connection.GetPoolsbyId().subscribe((data: any) => {
+      this.poolDetails = data;
+      console.warn(this.poolDetails)
+    })
   }
 
+  filterDropdown() {
+  
+    this.drive = [];
+  
+    for (let item of this.Utilized) {
+      if ( this._pool == '' && item.interviewDate <= this._date ) {
+        this.drive.push(item);
+      }
+      else if (item.drivePool == this._pool && this._date <= item.interviewDate ) {
+        this.drive.push(item);
+      }
+      else if (this._pool == '' && this._date == '') {
+        this.drive.push(item);
+      }
+      else if (item.drivePool == this._pool && this._date == '') {
+        this.drive.push(item);
+      }
+    }
+  }
 }
