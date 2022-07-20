1da2babf5aa8292cd5f14948e626cc202d7cd387
KumareshKS <102685800+KumareshKS@users.noreply.github.com>
2022-07-20 15:46:40 +0530

    Update admin-view-requests-page.component.ts

--- Source/Angular/IMS/src/app/Admin/admin-view-requests-page/admin-view-requests-page.component.ts
+++ Source/Angular/IMS/src/app/Admin/admin-view-requests-page/admin-view-requests-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ConnectionService } from 'src/app/Services/connection.service';
 import { AuthenticationService } from 'src/app/Services/authentication.service';
+import { DialogueBoxService } from 'src/app/Services/dialogue-box.service';
 
 @Component({
   selector: 'app-admin-view-requests-page',
@@ -17,19 +18,29 @@ export class AdminViewRequestsPageComponent implements OnInit {
   title = "Employee Requests";
   result: any;
 
-  constructor(private service: ConnectionService) { }
+  constructor(private service: ConnectionService, private dialogueService: DialogueBoxService) { }
 
   ngOnInit(): void {
     this.GetEmployeeRequests();
   }
   //THIS METHOD IS CALLED ON CLICK0F ACCEPT OR REJECT BUTTON
-  RespondEmployeeRequest(employeeId: number, response: boolean) {
-    this.service.RespondEmployeeRequest(employeeId, response).subscribe({
-      next: (data) => { this.response = data.message },
-      error: (error) => { this.error = error.error, this.snackBar() },
-      complete: () => { this.snackBar(), this.GetEmployeeRequests() }
-    });
-
+  async RespondEmployeeRequest(employeeId: number, response: boolean) {
+    if (response == false)
+      await this.dialogueService.IsDeleteConfirmed().then((value) => {
+        if (value)
+          this.service.RespondEmployeeRequest(employeeId, response).subscribe({
+            next: (data) => { this.response = data.message },
+            error: (error) => { this.error = error.error, this.snackBar() },
+            complete: () => { this.snackBar(), this.GetEmployeeRequests() }
+          })
+      });
+    else {
+      this.service.RespondEmployeeRequest(employeeId, response).subscribe({
+        next: (data) => { this.response = data.message },
+        error: (error) => { this.error = error.error, this.snackBar() },
+        complete: () => { this.snackBar(), this.GetEmployeeRequests() }
+      })
+    }
   }
   //GET METHOD CALLED AT CONNECTION SERVICE
   GetEmployeeRequests() {
