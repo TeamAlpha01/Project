43b845d3435ff48f5095a9199c607b9087132d10
sheik731 <98314037+sheik731@users.noreply.github.com>
2022-07-21 10:40:17 +0530

    Fixed some validation Bugs

--- Source/InterviewManagementSystemAPI/IMS.API/Validations/DriveValidation.cs
+++ Source/InterviewManagementSystemAPI/IMS.API/Validations/DriveValidation.cs
@@ -12,11 +12,11 @@ namespace IMS.Validations
             int maximumFromDate = Convert.ToInt32(configuration.GetSection("DriveSettings").GetSection("MaximumFromDate").Value); 
             int drivePeriod = Convert.ToInt32(configuration.GetSection("DriveSettings").GetSection("DrivePeriod").Value); 
 
-            if (drive == null) throw new ValidationException("drive object cannot be null");
+            if (string.IsNullOrWhiteSpace(drive.Name)) throw new ValidationException("Drive name cannot be null");
 
-            if (drive.Name!.Length <= 2) throw new ValidationException("drive name is too short");
+            if (drive.Name!.Length <= 2) throw new ValidationException("Drive name is too short");
 
-            if(!Regex.IsMatch(drive.Name,"^[a-zA-Z0-9 ]{2,20}$")) throw new ValidationException("drive name cannot contain symbols");
+            if(!Regex.IsMatch(drive.Name,"^[a-zA-Z0-9 ]{2,20}$")) throw new ValidationException("Drive name cannot contain symbols");
 
             if (drive.FromDate.Date < System.DateTime.Now.Date.AddDays(minimumFromDate)) throw new ValidationException($"From date must be greater than {System.DateTime.Now.Date.AddDays(minimumFromDate-1).ToShortDateString()}");
 
@@ -43,15 +43,15 @@ namespace IMS.Validations
         {
             IsDriveIdValid(driveId);
             IsEmployeeIdValid(tacId);   
-            if(String.IsNullOrEmpty(reason)) throw new ValidationException("reason cannot be empty");
+            if(String.IsNullOrEmpty(reason)) throw new ValidationException("Reason cannot be empty");
             if(reason.Length<=5) throw new ValidationException($"{reason} : This reason is too short");
-            if(String.IsNullOrWhiteSpace(reason)) throw new ValidationException("reason cannot contain only whitespaces");
-            if(!Regex.IsMatch(reason,"[a-zA-Z0-9]")) throw new ValidationException($"{reason} : reason cannot contain only symbols");
+            if(String.IsNullOrWhiteSpace(reason)) throw new ValidationException("Reason cannot contain only whitespaces");
+            if(!Regex.IsMatch(reason,"[a-zA-Z0-9]")) throw new ValidationException($"{reason} : Reason cannot contain only symbols");
 
         }
         public static void IsDriveIdValid(int driveId)
         {
-            if (driveId <= 0) throw new ValidationException("drive id is invalid");
+            if (driveId <= 0) throw new ValidationException("Drive id is invalid");
         }
         public static void IsEmployeeIdValid(int employeeId)
         {
