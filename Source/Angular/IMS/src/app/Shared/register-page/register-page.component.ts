8be0bfc997aa85c8b8a6689cbfc307ef6f74216d
DarshanaAnnappan <darshana0175@gmail.com>
2022-07-21 15:30:41 +0530

    Updated register page

--- Source/Angular/IMS/src/app/Shared/register-page/register-page.component.ts
+++ Source/Angular/IMS/src/app/Shared/register-page/register-page.component.ts
@@ -29,7 +29,7 @@ export class RegisterPageComponent implements OnInit {
   ngOnInit(): void {
 
     this.registerForm = this.FB.group({
-      Name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(25), Validators.pattern("^(?!.*([ ])\\1)(?!.*([A-Za-z])\\2{2})\\w[a-zA-Z\\s]*$")]],
+      Name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern("^(?!.*([ ])\\1)(?!.*([A-Za-z])\\2{2})\\w[a-zA-Z\\s]*$")]],
       ACENumber: ['', [Validators.required, Validators.pattern("^ACE[0-9]{4,5}$"), Validators.pattern("^(?!.*ACE0000).*$"), Validators.pattern("^(?!.*ACE00000).*$")]],
       Department: ['', [Validators.required]],
       Role: [{ value: '', disabled: true }, [Validators.required]],
